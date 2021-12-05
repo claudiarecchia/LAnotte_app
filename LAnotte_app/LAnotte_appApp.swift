3ee249eda3cfd44169f8379f4ec195d7719cc59a
Claudia <claudiarecchia97@gmail.com>
2021-12-05 10:20:46 +0100

    Initial Commit

---
+++ LAnotte_app/LAnotte_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  LAnotte_appApp.swift
+//  LAnotte_app
+//
+//  Created by Claudia Recchia on 05/12/21.
+//
+
+import SwiftUI
+
+@main
+struct LAnotte_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LAnotte_appUITests/LAnotte_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LAnotte_appUITestsLaunchTests.swift
+//  LAnotte_appUITests
+//
+//  Created by Claudia Recchia on 05/12/21.
+//
+
+import XCTest
+
+class LAnotte_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
